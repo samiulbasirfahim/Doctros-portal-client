cbedb21816232adfa2d499d23050e471dc57fda3
samiulbasirfahim <samiulbasirfahim360@gmail.com>
2022-05-10 06:15:17 +0600

    appoinment seciton done

---
+++ src/Pages/Home/Appoinments.js
@@ -0,0 +1,40 @@
+import React from "react"
+import ButtonPrimary from "../../Components/ButtonPrimary"
+import doctor from "../../assets/images/doctor-small.png"
+import appoinmentsbg from "../../assets/images/appointment.png"
+
+const Appoinments = () => {
+	return (
+		<section
+			style={{
+				background: `url(${appoinmentsbg})`,
+			}}
+		>
+			<div className=" flex mt-40 flex-col lg:flex-row justify-center items-center px-6">
+				<div className="flex-1 hidden lg:flex justify-end">
+					<img
+						className="mt-[-200px]"
+						src={doctor}
+						alt=""
+					/>
+				</div>
+				<div className="flex-1 flex flex-col justify-center  py-8 items-start">
+					<p className="text-secondary text-xl font-bold">Appointment</p>
+					<p className="text-4xl text-white font-bold">Make an appointment Today</p>
+					<p className="text-white mb-4">
+						It is a long established fact that a reader will be
+						distracted by the readable content of a page when
+						looking at its layout. The point of using Lorem Ipsumis
+						that it has a more-or-less normal distribution of
+						letters,as opposed to using 'Content here, content
+						here', making it look like readable English. Many
+						desktop publishing packages and web page
+					</p>
+					<ButtonPrimary>Get started</ButtonPrimary>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Appoinments
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,33 +5,37 @@ import clock from "../../assets/icons/clock.svg"
 import marker from "../../assets/icons/marker.svg"
 import phone from "../../assets/icons/phone.svg"
 import Services from "./Services"
+import Appoinments from "./Appoinments"
 
 const Home = () => {
 	return (
-		<section className="container px-[25px] mx-auto">
+		<>
 			<Banner />
-			<div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-				<BannerCard
-					img={clock}
-					title="opening hour"
-					description="lorem ipsum dolor sit amet, consectetur adip"
-					bg="bg-gradient-to-l from-primary to-secondary"
-				></BannerCard>
-				<BannerCard
-					img={marker}
-					title="opening hour"
-					description="lorem ipsum dolor sit amet, consectetur adip"
-					bg="bg-accent"
-				></BannerCard>
-				<BannerCard
-					img={phone}
-					title="opening hour"
-					description="lorem ipsum dolor sit amet, consectetur adip"
-					bg="bg-gradient-to-l from-primary to-secondary"
-				></BannerCard>
-			</div>
-			<Services />
-		</section>
+			<section className="container px-[25px] mx-auto">
+				<div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+					<BannerCard
+						img={clock}
+						title="opening hour"
+						description="lorem ipsum dolor sit amet, consectetur adip"
+						bg="bg-gradient-to-l from-primary to-secondary"
+					></BannerCard>
+					<BannerCard
+						img={marker}
+						title="opening hour"
+						description="lorem ipsum dolor sit amet, consectetur adip"
+						bg="bg-accent"
+					></BannerCard>
+					<BannerCard
+						img={phone}
+						title="opening hour"
+						description="lorem ipsum dolor sit amet, consectetur adip"
+						bg="bg-gradient-to-l from-primary to-secondary"
+					></BannerCard>
+				</div>
+				<Services />
+			</section>
+			<Appoinments />
+		</>
 	)
 }
 
