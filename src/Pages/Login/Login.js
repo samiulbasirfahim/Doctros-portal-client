e803bc8920bfb58c63f5305b67f839876b79f22a
samiulbasirfahim <samiulbasirfahim360@gmail.com>
2022-05-11 20:08:29 +0600

    login page done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
 
 const Login = () => {
-    return (
-        <div className='h-screen w-screen flex justify-center items-center'>
-            <p className='text'>
-                this is a text
-            </p>
-        </div>
-    );
-};
+	const [showPass, setShowPass] = useState(false)
+	return (
+		<div className="min-h-screen w-screen flex justify-center items-center">
+			<div className="shadow-2xl py-12 px-8 rounded-2xl lg:w-3/12 ">
+				<form className="grid grid-cols-1 gap-y-4">
+					<p className="text-center w-full pb-8 font-bold text-xl">
+						Login
+					</p>
+					<p className="flex flex-col">
+						<label htmlFor="email">Email</label>
+						<input
+							type="email"
+							name="email"
+							id="email"
+							class="input input-bordered w-full"
+						/>
+					</p>
+					<p className="flex flex-col">
+						<label htmlFor="password">Password</label>
+						<input
+							type={showPass ? "text" : "password"}
+							name="password"
+							id="password"
+							class="input input-bordered w-ful"
+						/>
+					</p>
+					<Link to="/reset-password" className="text-xs">
+						Forget password ?
+					</Link>
+					<input type="submit" value="Login" className="btn w-full" />
+				</form>
+				<p className="text-sm text-center mt-2">
+					New to doctors portal ?   
+					<Link to="/sign-up" className="text-sm text-secondary ml-2">
+						Create new account
+					</Link>
+				</p>
+				<div class="divider mt-4">OR</div>
+			</div>
+		</div>
+	)
+}
 
-export default Login;
+export default Login
